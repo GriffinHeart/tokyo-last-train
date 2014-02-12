e3cce7a72b89ceba12eb9f629560b6b881b49506
yolandamcorreia <yolanda.mcorreia@gmail.com>
2014-02-12 18:44:55 +0900

    Removed scrollbars on splash screen

--- src/com/tokyolasttrain/view/gif/GifWebView.java
+++ src/com/tokyolasttrain/view/gif/GifWebView.java
@@ -1,6 +1,8 @@
 package com.tokyolasttrain.view.gif;
 
 import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebView;
 
@@ -19,6 +21,17 @@ public class GifWebView extends WebView
 	
 	public void fitScreen()
 	{
+		setVerticalScrollBarEnabled(false);
+		setHorizontalScrollBarEnabled(false);
+
 		getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
+		setOnTouchListener(new OnTouchListener()
+		{
+			@Override
+			public boolean onTouch(View v, MotionEvent e)
+			{
+				return (e.getAction() == MotionEvent.ACTION_MOVE);
+			}
+		});
 	}
 }
